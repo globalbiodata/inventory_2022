5ea0cbd7cce877218dee994c12ce7cbb176295d9
Kenneth Schackart <schackartk1@gmail.com>
2022-03-04 09:10:23 -0700

    move to get_args and group

---
+++ tests/train_test.py
@@ -0,0 +1,27 @@
+""" Tests """
+
+import os
+# import random
+# import re
+# import shutil
+# import string
+from subprocess import getstatusoutput
+
+PRG = './train.py'
+
+
+# ---------------------------------------------------------------------------
+def test_exists():
+    """ Program exists """
+
+    assert os.path.isfile(PRG)
+
+
+# ---------------------------------------------------------------------------
+def test_usage():
+    """ Usage """
+
+    for flag in ['-h', '--help']:
+        retval, out = getstatusoutput(f'{PRG} {flag}')
+        assert retval == 0
+        assert out.lower().startswith('usage')
