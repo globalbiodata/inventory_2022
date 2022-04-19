d75bf19a43cab6906b62fc8766a692734a9d18c0
Kenneth Schackart <schackartk1@gmail.com>
2022-04-19 10:33:20 -0700

    initial rework of class_data_generator

--- src/class_data_generator.py
+++ src/class_data_generator.py
@@ -1,58 +1,268 @@
-from sklearn.model_selection import train_test_split
-import pandas as pd
+#!/usr/bin/env python3
+"""
+Purpose: Split curated data into training, validation, and testing sets
+Authors: Ana-Maria Istrate and Kenneth Schackart
+"""
+
 import argparse
-import numpy as np
+import os
+import pytest
+import sys
+from typing import List, NamedTuple, TextIO
 
-RND_SEED = 241
+import pandas as pd
+from pandas.testing import assert_frame_equal
+from sklearn.model_selection import train_test_split
+
+from utils import CustomHelpFormatter
 
 
-def get_splits_from_df(df):
+# ---------------------------------------------------------------------------
+class Args(NamedTuple):
     """
-  Splits manual curation df into train, val and test splits
-  RND_SEED is set for reproducibility
-  """
-    df_certain = df[df['curation_score'].isin([0, 1])]
-    assert df_certain['id'].count() == df_certain['id'].nunique()
-    np.random.seed(RND_SEED)
-    sent_ids = df_certain['id'].unique()
-    sent_ids_train, sent_ids_val_test = train_test_split(sent_ids,
-                                                         test_size=0.3,
-                                                         random_state=RND_SEED)
-    sent_ids_val, sent_ids_test = train_test_split(sent_ids_val_test,
-                                                   test_size=0.5,
-                                                   random_state=RND_SEED)
+    Command-line arguments
 
-    train_df = df_certain[df_certain['id'].isin(sent_ids_train)]
-    val_df = df_certain[df_certain['id'].isin(sent_ids_val)]
-    test_df = df_certain[df_certain['id'].isin(sent_ids_test)]
-    return train_df, val_df, test_df
+    `infile`: Input curated data filehandle
+    `outdir`: Output directory
+    `train`: Training data output file name
+    `val`: Validation data output file name
+    `spltis`: Train, val, test proportions
+    `test`: Test data output file name
+    `seed`: Random seed
+    """
+    infile: TextIO
+    outdir: str
+    train: str
+    val: str
+    test: str
+    splits: List[float]
+    seed: bool
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+# ---------------------------------------------------------------------------
+class Splits(NamedTuple):
+    """
+    Training, validation, and test dataframes
 
-    parser.add_argument('--input-file',
-                        type=str,
+    `train`: Training data
+    `val`: Validation data
+    `test`: Test data
+    """
+    train: pd.DataFrame
+    val: pd.DataFrame
+    test: pd.DataFrame
+
+
+# ---------------------------------------------------------------------------
+@pytest.fixture(name='unsplit_data')
+def fixture_unsplit_data() -> pd.DataFrame:
+    """ Example dataframe for testing splitting function """
+
+    df = pd.DataFrame([[123, 'First title', 'First abstract', 0],
+                       [456, 'Second title', 'Second abstract', 1],
+                       [789, 'Third title', 'Third abstract', 0],
+                       [321, 'Fourth title', 'Fourth abstract', 1],
+                       [654, 'Fifth title', 'Fifth abstract', 0],
+                       [987, 'Sixth title', 'Sixth abstract', 1],
+                       [741, 'Seventh title', 'Seventh abstract', 0],
+                       [852, 'Eighth title', 'Eighth abstract', 1]],
+                      columns=['id', 'title', 'abstract', 'curation_score'])
+
+    return df
+
+
+# ---------------------------------------------------------------------------
+def get_args() -> Args:
+    """ Parse command-line arguments """
+
+    parser = argparse.ArgumentParser(
+        description='Split curated classification data',
+        formatter_class=CustomHelpFormatter)
+
+    parser.add_argument('infile',
+                        metavar='FILE',
+                        type=argparse.FileType('rt', encoding='ISO-8859-1'),
                         default='data/manual_classifications.csv',
-                        help='Location of the input file')
-    parser.add_argument('--train-file',
+                        help='Manually classified input file')
+    parser.add_argument('-o',
+                        '--outdir',
+                        metavar='',
                         type=str,
-                        default='data/train_paper_classif.csv',
-                        help='Location where the training file will be saved')
-    parser.add_argument('--val-file',
+                        default='data/',
+                        help='Output directory')
+    parser.add_argument('-t',
+                        '--train',
+                        metavar='',
                         type=str,
-                        default='data/val_paper_classif.csv',
-                        help='Location where the val file will be saved')
-    parser.add_argument('--test-file',
+                        default='train_paper_classif.csv',
+                        help='Training data output file name')
+    parser.add_argument('-v',
+                        '--val',
+                        metavar='',
                         type=str,
-                        default='data/test_paper_classif.csv',
-                        help='Location where the test file will be saved')
+                        default='val_paper_classif.csv',
+                        help='Validation data output file name')
+    parser.add_argument('-s',
+                        '--test',
+                        metavar='',
+                        type=str,
+                        default='test_paper_classif.csv',
+                        help='Test data output file name')
+    parser.add_argument('--splits',
+                        metavar='',
+                        type=float,
+                        nargs=3,
+                        default=[0.7, 0.15, 0.15],
+                        help='Proportions for train, val, test splits')
+    parser.add_argument('-r',
+                        '--seed',
+                        action='store_true',
+                        help='Set random seed')
 
-    args, _ = parser.parse_known_args()
-    print(f'args={args}')
+    args = parser.parse_args()
 
-    df = pd.read_csv(args.input_file)
-    train_df, val_df, test_df = get_splits_from_df(df)
-    train_df.to_csv(args.train_file, index=False)
-    val_df.to_csv(args.val_file, index=False)
-    test_df.to_csv(args.test_file, index=False)
+    if not sum(args.splits) == 1.0:
+        parser.error(f'--splits {args.splits} must sum to 1')
+
+    return Args(args.infile, args.outdir, args.train, args.val, args.test,
+                args.splits, args.seed)
+
+
+# ---------------------------------------------------------------------------
+def filter_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Return only data with curation score of 0 or 1
+
+    `df`: Manually curated data
+    """
+
+    df = df[['id', 'title', 'abstract', 'curation_score']]
+
+    return df[df['curation_score'].isin([0, 1])]
+
+
+# ---------------------------------------------------------------------------
+def test_filter_data() -> None:
+    """ Test filter_data() """
+
+    in_df = pd.DataFrame(
+        [[123, 'First title', 'First abstract', 0, 'nope'],
+         [456, 'Second title', 'Second abstract', 1, 'yup'],
+         [789, 'Third title', 'Third abstract', 0.5, 'unsure']],
+        columns=['id', 'title', 'abstract', 'curation_score', 'notes'])
+
+    out_df = pd.DataFrame(
+        [[123, 'First title', 'First abstract', 0],
+         [456, 'Second title', 'Second abstract', 1]],
+        columns=['id', 'title', 'abstract', 'curation_score'])
+
+    assert_frame_equal(filter_data(in_df), out_df, check_dtype=False)
+
+
+# ---------------------------------------------------------------------------
+def check_data(df: pd.DataFrame) -> None:
+    """ Check that input data is valid """
+
+    num_certain = df['id'].count()
+    unique_ids = df['id'].nunique()
+
+    if not num_certain == unique_ids:
+        sys.exit(f'Number of certain scores ({num_certain}) not equal to'
+                 f'number of unique IDs ({unique_ids}).')
+
+
+# ---------------------------------------------------------------------------
+def split_df(df: pd.DataFrame, rand_seed: bool, splits: List[float]) -> Splits:
+    """
+    Split manually curated data into train, validation and test sets
+    
+    `df`: Manually curated classification data
+    `seed`: Optionally use random seed
+    """
+
+    seed = 241 if rand_seed else None
+
+    _, val_split, test_split = splits
+    val_test_split = val_split + test_split
+
+    train, val_test = train_test_split(df,
+                                       test_size=val_test_split,
+                                       random_state=seed)
+    val, test = train_test_split(val_test,
+                                 test_size=test_split / val_test_split,
+                                 random_state=seed)
+
+    return Splits(train, val, test)
+
+
+# ---------------------------------------------------------------------------
+def test_random_split(unsplit_data: pd.DataFrame) -> None:
+    """ Test that split_df() gives correct proportions """
+
+    in_df = unsplit_data
+
+    train, val, test = split_df(in_df, False, [0.5, 0.25, 0.25])
+
+    assert len(train.index) == 4
+    assert len(val.index) == 2
+    assert len(test.index) == 2
+
+
+# ---------------------------------------------------------------------------
+def test_seeded_split(unsplit_data: pd.DataFrame) -> None:
+    """ Test that split_df() behaves deterministically """
+
+    in_df = unsplit_data
+
+    train, val, test = split_df(in_df, True, [0.5, 0.25, 0.25])
+
+    assert list(train['id'].values) == [321, 789, 741, 654]
+    assert list(val['id'].values) == [987, 456]
+    assert list(test['id'].values) == [852, 123]
+
+
+# ---------------------------------------------------------------------------
+def make_filename(dir: str, name: str) -> str:
+    """
+    Make output filename
+
+    `dir`: Output directory
+    `name`: File name
+    """
+
+    outfile = os.path.join(dir, name)
+
+    return outfile
+
+
+# ---------------------------------------------------------------------------
+def main() -> None:
+    """ Main function """
+
+    args = get_args()
+    out_dir = args.outdir
+
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+
+    df = pd.read_csv(args.infile)
+
+    df = filter_data(df)
+
+    check_data(df)
+
+    train_df, val_df, test_df = split_df(df, args.seed, args.splits)
+
+    train_out, val_out, test_out = map(lambda f: make_filename(out_dir, f),
+                                       [args.train, args.val, args.test])
+
+    train_df.to_csv(train_out, index=False)
+    val_df.to_csv(val_out, index=False)
+    test_df.to_csv(test_out, index=False)
+
+    print(f'Done. Wrote 3 files to {out_dir}.')
+
+
+# ---------------------------------------------------------------------------
+if __name__ == '__main__':
+    main()
