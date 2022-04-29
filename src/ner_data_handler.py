03d845948cccd294d9c6bcdfcb61bf029bac13d3
Kenneth Schackart <schackartk1@gmail.com>
2022-04-29 15:16:58 -0700

    begin splitting into functions

--- src/ner_data_handler.py
+++ src/ner_data_handler.py
@@ -3,13 +3,105 @@ Purpose: Preprocess and tokenize data, create DataLoader
 Authors: Ana-Maria Istrate and Kenneth Schackart
 """
 
+from typing import NamedTuple, TextIO
+
 from datasets import load_dataset
 from torch.utils.data import DataLoader
-from transformers import AutoTokenizer, DataCollatorForTokenClassification
+from transformers import (AutoTokenizer, DataCollatorForTokenClassification,
+                          PreTrainedTokenizer)
 
 from utils import NER_TAG2ID
 
 
+# ---------------------------------------------------------------------------
+class RunParams(NamedTuple):
+    """
+    Model and run parameters
+
+    `model_name`: Huggingface model name
+    `batch_size`: Tokenization batch size
+    `sanity_check`: Run on smaller subset of data
+    """
+    model_name: str
+    batch_size: int
+    sanity_check: bool = False
+
+
+# ---------------------------------------------------------------------------
+def get_dataloader(file: TextIO, run_params: RunParams) -> DataLoader:
+    """
+    Preprocess data and create dataloader
+
+    Parameters:
+    `file`: Input file handle
+    `run_params`: Model and run parameters
+
+    Returns:
+    A `DataLoader` with preprocessed data
+    """
+
+    dataset = load_dataset('pandas', data_files=file)
+
+    tokenizer = AutoTokenizer.from_pretrained(run_params.model_name)
+    collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
+
+    tokenized_dataset = tokenize_align_labels(dataset, tokenizer)
+
+    if run_params.sanity_check:
+        tokenized_datasets = tokenized_datasets.select(range(100))
+
+    dataloader = DataLoader(tokenized_dataset,
+                            shuffle=True,
+                            collate_fn=collator,
+                            batch_size=run_params.batch_size)
+
+    return dataloader
+
+
+# ---------------------------------------------------------------------------
+def tokenize_align_labels(dataset, tokenizer: PreTrainedTokenizer):
+
+    tokenized_inputs = tokenizer(dataset['words'],
+                                 truncation=True,
+                                 is_split_into_words=True)
+
+    new_labels = []
+
+    # Should be able to use zip instead of enumerate
+    for i, labels in enumerate(dataset['words']):
+        labels = [NER_TAG2ID[x] for x in labels]
+        word_ids = tokenized_inputs.word_ids(i)
+        new_labels.append(align_labels_with_tokens(labels, word_ids))
+
+    tokenized_inputs['labels'] = new_labels
+
+    return tokenized_inputs
+
+
+# ---------------------------------------------------------------------------
+def align_labels_with_tokens(labels, word_ids, cls_token=-100):
+
+    new_labels = []
+    last_word_id = None
+    last_tag = None
+    for word_id in word_ids:
+        if word_id is None:
+            curr_tag = cls_token
+        elif word_id == last_word_id:
+            if last_tag % 2 == 1:
+                curr_tag = last_tag + 1
+            else:
+                curr_tag = last_tag
+        else:
+            curr_tag = labels[word_id]
+            last_tag = curr_tag
+        new_labels.append(curr_tag)
+        last_word_id = word_id
+
+    return new_labels
+
+
+# ---------------------------------------------------------------------------
 class NERDataHandler:
     """
     Handles generating training, validation and testing dataloaders used for training and evaluation
