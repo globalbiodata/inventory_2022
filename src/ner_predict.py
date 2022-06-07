dd684f217d2d44a60828a2995a60c1badb6cf9ff
Kenneth Schackart <schackartk1@gmail.com>
2022-06-07 15:42:55 -0700

    remove unused index

--- src/ner_predict.py
+++ src/ner_predict.py
@@ -275,15 +275,13 @@ def predict(model, tokenizer: PreTrainedTokenizer, inputs: pd.DataFrame,
         probs = [x.prob for x in predicted_labels]
         pred_df = pd.concat([
             pred_df,
-            pd.DataFrame(
-                {
-                    'ID': [row['id']] * num_preds,
-                    'text': [seq] * num_preds,
-                    'mention': mentions,
-                    'label': labels,
-                    'prob': probs
-                },
-                index=[0])
+            pd.DataFrame({
+                'ID': [row['id']] * num_preds,
+                'text': [seq] * num_preds,
+                'mention': mentions,
+                'label': labels,
+                'prob': probs
+            })
         ])
 
     return pred_df
