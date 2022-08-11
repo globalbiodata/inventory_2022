12e31dbaccae578ef5d30349d9b7eb8a95e2a8c8
Kenneth Schackart <schackartk1@gmail.com>
2022-08-11 16:51:11 -0700

    add docstrings to constant dicts

--- src/utils.py
+++ src/utils.py
@@ -27,11 +27,21 @@ from transformers import AutoTokenizer
 from transformers.tokenization_utils import PreTrainedTokenizer
 
 # ---------------------------------------------------------------------------
-# Mapping from NER tag to ID
 NER_TAG2ID = {'O': 0, 'B-COM': 1, 'I-COM': 2, 'B-FUL': 3, 'I-FUL': 4}
+"""
+Mapping of NER tags to numerical labels
 
-# Mapping from ID to NER tag
+`key`: String NER label ("O", "B-COM", "I-COM", "B-FUL", "I-FUL")
+`value`: Numerical label (1, 2, 3, 4)
+"""
+
 ID2NER_TAG = {v: k for k, v in NER_TAG2ID.items()}
+"""
+Mapping of numerical labels to NER tags
+
+`key`: Numerical label (1, 2, 3, 4)
+`value`: String NER label ("O", "B-COM", "I-COM", "B-FUL", "I-FUL")
+"""
 
 
 # ---------------------------------------------------------------------------
