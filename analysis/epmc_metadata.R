7f740318e685e10326f97f88f4acda7b006890d8
Kenneth Schackart <schackartk1@gmail.com>
2023-01-19 11:12:22 -0700

    add figure generation

--- analysis/epmc_metadata.R
+++ analysis/epmc_metadata.R
@@ -14,11 +14,21 @@
 library(argparse)
 library(dplyr)
 library(europepmc)
+library(ggplot2)
 library(magrittr)
+library(RColorBrewer)
 library(readr)
-# library(reshape2)
+library(scales)
 library(stringr)
 library(tidyr)
+
+# Settings ------------------------------------------------------------------
+
+theme_set(theme_light() +
+            theme(
+              plot.title = element_text(hjust = 0.5),
+              plot.subtitle = element_text(hjust = 0.5)
+            ))
 
 # Function Definitions ------------------------------------------------------
 
@@ -152,7 +162,36 @@ long_inventory <- full_inventory %>%
 cat("Getting metadata from Europe PMC... ")
 
 metadata_df <- get_metadata(long_inventory$id)
+
 long_inventory <- full_join(long_inventory, metadata_df)
+
+resource_metadata <- long_inventory %>%
+  select(resource_num,
+         license,
+         isOpenAccess,
+         hasTextMinedTerms,
+         hasTMAccessionNumbers) %>%
+  mutate(license = case_when(!is.na(license) ~ "cc",
+                             T ~ "no")) %>%
+  aggregate(. ~ resource_num, ., unique) %>%
+  mutate(
+    license = case_when(license == "cc" ~ "cc",
+                        license == "no" ~ "no",
+                        T ~ "both"),
+    isOpenAccess = case_when(isOpenAccess == "Y" ~ "Y",
+                             isOpenAccess == "N" ~ "N",
+                             T ~ "both"),
+    hasTextMinedTerms = case_when(
+      hasTextMinedTerms == "Y" ~ "Y",
+      hasTextMinedTerms == "N" ~ "N",
+      T ~ "both"
+    ),
+    hasTMAccessionNumbers = case_when(
+      hasTMAccessionNumbers == "Y" ~ "Y",
+      hasTMAccessionNumbers == "N" ~ "N",
+      T ~ "both"
+    )
+  )
 
 cat("Done.\n")
 
@@ -174,6 +213,7 @@ summary <- tibble(
   type = character(),
   resources_yes = numeric(),
   resources_no = numeric(),
+  resources_mixed = numeric(),
   articles_yes = numeric(),
   articles_no = numeric()
 )
@@ -183,18 +223,49 @@ summary <- tibble(
 articles_w_full_text <- nrow(oa_ft_inventory)
 articles_wo_full_text <- nrow(id_list) - nrow(oa_ft_inventory)
 
+oa_ft_resources <- oa_ft_inventory %>%
+  mutate(oa_ft = "true") %>%
+  right_join(long_inventory) %>%
+  distinct(id, .keep_all = T) %>%
+  select(id, oa_ft, resource_num) %>%
+  mutate(oa_ft = case_when(is.na(oa_ft) ~ "false", T ~ oa_ft)) %>%
+  aggregate(. ~ resource_num, ., unique) %>%
+  mutate(oa_ft = case_when(oa_ft == "true" ~ "true",
+                           oa_ft == "false" ~ "false",
+                           T ~ "both")) %>%
+  group_by(oa_ft) %>%
+  summarize(count = n())
+
+ft_resources <- oa_ft_resources %>%
+  filter(oa_ft == "true") %>%
+  select(count)
+not_ft_resources <- oa_ft_resources %>%
+  filter(oa_ft == "false") %>%
+  select(count)
+mixed_ft_resources <- oa_ft_resources %>%
+  filter(oa_ft == "both") %>%
+  select(count)
+
 summary <- summary %>%
   rbind(
     tibble(
       type = "Full Text XML Available",
-      resources_yes = NA,
-      resources_no = NA,
+      resources_yes = ft_resources$count,
+      resources_no = not_ft_resources$count,
+      resources_mixed = mixed_ft_resources$count,
       articles_yes = articles_w_full_text,
       articles_no = articles_wo_full_text
     )
   )
 
-rm(articles_w_full_text, articles_wo_full_text)
+rm(
+  articles_w_full_text,
+  articles_wo_full_text,
+  oa_ft_resources,
+  ft_resources,
+  not_ft_resources,
+  mixed_ft_resources
+)
 
 
 ### License availability --------------------------------------------------
@@ -213,20 +284,42 @@ articles_wo_cc_license <- article_licenses %>%
   filter(has_license == "no") %>%
   select(count)
 
+resource_licenses <- resource_metadata %>%
+  select(license) %>%
+  group_by(license) %>%
+  summarize(count = n())
+
+resources_w_cc_license <- resource_licenses %>%
+  filter(license == "cc") %>%
+  select(count)
+resources_wo_cc_license <- resource_licenses %>%
+  filter(license == "no") %>%
+  select(count)
+resources_w_mixed_license <- resource_licenses %>%
+  filter(license == "both") %>%
+  select(count)
+
 summary <- summary %>%
   rbind(
     tibble(
-      type = "CC licensed",
-      resources_yes = NA,
-      resources_no = NA,
+      type = "CC Licensed",
+      resources_yes = resources_w_cc_license,
+      resources_no = resources_wo_cc_license,
+      resources_mixed = resources_w_mixed_license,
       articles_yes = articles_w_cc_license,
       articles_no = articles_wo_cc_license
     )
   )
 
-rm(article_licenses,
-   articles_w_cc_license,
-   articles_wo_cc_license)
+rm(
+  article_licenses,
+  articles_w_cc_license,
+  articles_wo_cc_license,
+  resource_licenses,
+  resources_w_cc_license,
+  resources_wo_cc_license,
+  resources_w_mixed_license
+)
 
 ### Open access -------------------------------------------------------------
 
@@ -242,20 +335,42 @@ not_open_access_articles <- article_access %>%
   filter(isOpenAccess == "N") %>%
   select(count)
 
+resource_access <- resource_metadata %>%
+  select(isOpenAccess) %>%
+  group_by(isOpenAccess) %>%
+  summarize(count = n())
+
+open_access_resources <- resource_access %>%
+  filter(isOpenAccess == "Y") %>%
+  select(count)
+not_open_access_resources <- resource_access %>%
+  filter(isOpenAccess == "N") %>%
+  select(count)
+mixed_access_resources <- resource_access %>%
+  filter(isOpenAccess == "both") %>%
+  select(count)
+
 summary <- summary %>%
   rbind(
     tibble(
       type = "Open Access",
-      resources_yes = NA,
-      resources_no = NA,
+      resources_yes = open_access_resources,
+      resources_no = not_open_access_resources,
+      resources_mixed = mixed_access_resources,
       articles_yes = open_access_articles,
       articles_no = not_open_access_articles
     )
   )
 
-rm(article_access,
-   open_access_articles,
-   not_open_access_articles)
+rm(
+  article_access,
+  open_access_articles,
+  not_open_access_articles,
+  resource_access,
+  open_access_resources,
+  not_open_access_resources,
+  mixed_access_resources
+)
 
 ### Text mined terms --------------------------------------------------------
 
@@ -271,12 +386,28 @@ no_text_mined_terms <- text_mined_terms %>%
   filter(hasTextMinedTerms == "N") %>%
   select(count)
 
+res_text_mined_terms <- resource_metadata %>%
+  select(hasTextMinedTerms) %>%
+  group_by(hasTextMinedTerms) %>%
+  summarize(count = n())
+
+res_has_text_mined_terms <- res_text_mined_terms %>%
+  filter(hasTextMinedTerms == "Y") %>%
+  select(count)
+res_no_text_mined_terms <- res_text_mined_terms %>%
+  filter(hasTextMinedTerms == "N") %>%
+  select(count)
+res_mixed_text_mined_terms <- res_text_mined_terms %>%
+  filter(hasTextMinedTerms == "both") %>%
+  select(count)
+
 summary <- summary %>%
   rbind(
     tibble(
-      type = "Text mined terms",
-      resources_yes = NA,
-      resources_no = NA,
+      type = "Text Mined Terms",
+      resources_yes = res_has_text_mined_terms,
+      resources_no = res_no_text_mined_terms,
+      resources_mixed = res_mixed_text_mined_terms,
       articles_yes = has_text_mined_terms,
       articles_no = no_text_mined_terms
     )
@@ -298,29 +429,119 @@ no_text_mined_acc_nums <- text_mined_acc_nums %>%
   filter(hasTMAccessionNumbers == "N") %>%
   select(count)
 
+res_text_mined_acc_nums <- resource_metadata %>%
+  select(hasTMAccessionNumbers) %>%
+  group_by(hasTMAccessionNumbers) %>%
+  summarize(count = n())
+
+res_has_text_minedacc_nums <- res_text_mined_acc_nums %>%
+  filter(hasTMAccessionNumbers == "Y") %>%
+  select(count)
+res_no_text_mined_acc_nums <- res_text_mined_acc_nums %>%
+  filter(hasTMAccessionNumbers == "N") %>%
+  select(count)
+res_mixed_text_mined_acc_nums <- res_text_mined_acc_nums %>%
+  filter(hasTMAccessionNumbers == "both") %>%
+  select(count)
+
 summary <- summary %>%
   rbind(
     tibble(
-      type = "Text mined accession numbers",
-      resources_yes = NA,
-      resources_no = NA,
+      type = "Text Mined Accession Numbers",
+      resources_yes = res_has_text_minedacc_nums,
+      resources_no = res_no_text_mined_acc_nums,
+      resources_mixed = res_mixed_text_mined_acc_nums,
       articles_yes = has_text_mined_acc_nums,
       articles_no = no_text_mined_acc_nums
     )
   )
 
-rm(text_mined_acc_nums,
-   has_text_mined_acc_nums,
-   no_text_mined_acc_nums)
+rm(
+  text_mined_acc_nums,
+  has_text_mined_acc_nums,
+  no_text_mined_acc_nums,
+  res_text_mined_acc_nums,
+  res_has_text_minedacc_nums,
+  res_no_text_minedacc_nums,
+  res_mixed_text_minedacc_nums
+)
 
 ### Summarization -----------------------------------------------------------
 
 summary <- summary %>%
-  mutate(articles_yes = as.numeric(articles_yes),
-         articles_no = as.numeric(articles_no)) %>%
-  mutate(article_percent = (articles_yes / (articles_yes + articles_no)) *
-           100)
+  mutate(
+    articles_yes = as.numeric(articles_yes),
+    articles_no = as.numeric(articles_no),
+    resources_yes = as.numeric(resources_yes),
+    resources_no = as.numeric(resources_no),
+    resources_mixed = as.numeric(resources_mixed)
+  ) %>%
+  mutate(
+    articles_percent_yes = (articles_yes / (articles_yes + articles_no)) *
+      100,
+    articles_percent_no = (articles_no / (articles_yes + articles_no)) *
+      100,
+    resources_percent_yes = (resources_yes / (
+      resources_yes + resources_no + resources_mixed
+    )) * 100,
+    resources_percent_no = (resources_no / (
+      resources_yes + resources_no + resources_mixed
+    )) * 100,
+    resources_percent_mixed = (
+      resources_mixed / (resources_yes + resources_no + resources_mixed)
+    ) * 100
+  )
+
+summary_long <- summary %>%
+  select(type, contains("percent")) %>%
+  pivot_longer(cols = contains("percent"),
+               names_to = "asset_label",
+               values_to = "percent") %>%
+  mutate(asset_label = str_remove(asset_label, "_percent")) %>%
+  separate(asset_label, into = c("asset", "label")) %>%
+  mutate(
+    type = factor(
+      type,
+      levels = c(
+        "Text Mined Accession Numbers",
+        "Text Mined Terms",
+        "Full Text XML Available",
+        "Open Access",
+        "CC Licensed"
+      )
+    ),
+    asset = str_to_title(asset),
+    label = str_to_title(label),
+    label = factor(label, levels = c("No", "Mixed", "Yes"))
+  )
+
+### Visualization -----------------------------------------------------------
+
+summary_plot <- summary_long %>%
+  ggplot(aes(x = percent / 100, y = type, fill = label)) +
+  facet_wrap(~ asset) +
+  geom_col(width = 0.5) +
+  scale_fill_manual(values = c("#D95F02", "#666666", "#7570B3")) +
+  scale_x_continuous(labels = percent) +
+  labs(x = "",
+       y = "",
+       fill = "") +
+  guides(fill = guide_legend(reverse = T)) +
+  theme(legend.position = "bottom")
 
 ## Output -------------------------------------------------------------------
 
 write_csv(summary, file.path(out_dir, "text_mining_potential.csv"))
+
+ggsave(
+  file.path(out_dir, "text_mining_potential_plot.png"),
+  summary_plot,
+  width = 6.5,
+  height = 4
+)
+ggsave(
+  file.path(out_dir, "text_mining_potential_plot.svg"),
+  summary_plot,
+  width = 6.5,
+  height = 4
+)
